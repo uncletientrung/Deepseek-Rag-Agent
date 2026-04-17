d40caf72059158d81c3ae3d74028883d489ec17c
uncletientrung <thihachcf@gmail.com>
2026-04-17 22:40:20 +0700

    create Chat

--- rag_app/urls.py
+++ rag_app/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('history/', views.get_chat_history),
     path('delete-chat/', views.delete_chat),
     path('switch-document/', views.switch_document),
+    path('create-chat/', views.upload_pdf),
     # user sẽ gửi request -> Django nhận request -> Tạo HttpRequest -> tìm hàm và nhét request mặc định vào
 ]
